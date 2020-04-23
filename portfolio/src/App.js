69bd453033a1527ca4860bf33de35f712894be2a
Raine Wallace <rainebeaudubs@gmail.com>
2020-04-23 17:25:09 -0400

    added very simple nav component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <h1>Welcome to my portfolio!</h1>
     </div>
   );
---
+++ portfolio/src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <div>
+      <p>My nav will go here</p>
+    </div>
+  )
+}
+
+export default Navigation;
