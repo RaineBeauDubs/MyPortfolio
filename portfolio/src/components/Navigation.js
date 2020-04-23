186a7e7511e031873a21f24002a961bbd7963071
Raine Wallace <rainebeaudubs@gmail.com>
2020-04-23 18:24:21 -0400

    added very simple nav and minimal styling

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './componentCSS/navigation.css';
 
 const Navigation = () => {
   return (
-    <div>
-      <p>My nav will go here</p>
+    <div className="nav">
+      <NavLink className="navLink" to="/">
+        <h3>Introduction</h3>
+      </NavLink>
+      <NavLink className="navLink" to="/">
+        <h3>Experience</h3>
+      </NavLink>
+      <NavLink className="navLink" to="/">
+        <h3>Projects</h3>
+      </NavLink>
+      <NavLink className="navLink" to="/">
+        <h3>Contact</h3>
+      </NavLink>
     </div>
   )
 }
