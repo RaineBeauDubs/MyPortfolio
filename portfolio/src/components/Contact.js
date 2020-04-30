855574832505215893acbd5f1a50e1958235516d
Raine Wallace <rainebeaudubs@gmail.com>
2020-04-30 18:19:43 -0400

    added unstyled form and contact section

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -5,6 +5,40 @@ const Contact = () => {
   return (
     <div>
       <h3>Get In Touch!</h3>
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            type="text"
+            placeholder="Message..."
+          />
+          <div>
+            <button>Send</button>
+            <button>Clear</button>
+          </div>
+        </form>
+        <div>
+            <div>
+              <h4>Email</h4>
+              <p>RaineBeauDubs@gmail.com</p>
+            </div>
+            <div>
+              <h4>GitHub</h4>
+              <p>RaineBeauDubs</p>
+            </div>
+            <div>
+              <h4>LinkedIn</h4>
+              <p>/raine-wallace</p>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
