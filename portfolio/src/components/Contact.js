beed3fe79a221fc31757ad897c459b8091280896
Raine Wallace <rainebeaudubs@gmail.com>
2020-05-11 17:17:57 -0400

    styled on-site contact box

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -5,38 +5,41 @@ const Contact = () => {
   return (
     <div>
       <h3>Get In Touch!</h3>
-      <div>
-        <form>
-          <input
-            type="text"
-            placeholder="Name"
-          />
-          <input
-            type="text"
-            placeholder="Email"
-          />
-          <input
-            type="text"
+      <div className="flexSec">
+        <form className="box">
+          <div className="boxTop">
+            <input
+              type="text"
+              placeholder="Name"
+            />
+            <input
+              type="text"
+              placeholder="Email"
+            />
+          </div> 
+          <textarea
+            className="boxBttm"
+            type="textarea"
             placeholder="Message..."
           />
-          <div>
+          <div className="boxBttns">
             <button>Send</button>
             <button>Clear</button>
           </div>
         </form>
         <div>
-            <div>
-              <h4>Email</h4>
-              <p>RaineBeauDubs@gmail.com</p>
-            </div>
-            <div>
-              <h4>GitHub</h4>
-              <a href="https://github.com/RaineBeauDubs">RaineBeauDubs</a>
-            </div>
-            <div>
-              <h4>LinkedIn</h4>
-              <a href="https://www.linkedin.com/in/raine-wallace/">/raine-wallace</a>
-            </div>
+          <div>
+            <h4>Email</h4>
+            <p>RaineBeauDubs@gmail.com</p>
+          </div>
+          <div>
+            <h4>GitHub</h4>
+            <a href="https://github.com/RaineBeauDubs">RaineBeauDubs</a>
+          </div>
+          <div>
+            <h4>LinkedIn</h4>
+            <a href="https://www.linkedin.com/in/raine-wallace/">/raine-wallace</a>
+          </div>
         </div>
       </div>
     </div>
