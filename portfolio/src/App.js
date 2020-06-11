7581ca18f4ef8d83e854ec9ad2f063d9b973ed19
Raine Wallace <rainebeaudubs@gmail.com>
2020-06-11 12:36:13 -0400

    wired up super simple intro comp/css

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navigation from './components/Navigation';
+import Introduction from './components/Introduction';
 import Contact from './components/Contact';
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navigation />
       <h1>Welcome to my portfolio!</h1>
+      <Introduction />
       <Contact />
     </div>
   );
---
+++ portfolio/src/components/Introduction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './componentCSS/introduction.css';
+
+const Introduction = () => {
+  return (
+    <div>
+      <p>This will be my introduction!</p>
+    </div>
+  )
+}
+
+export default Introduction;
