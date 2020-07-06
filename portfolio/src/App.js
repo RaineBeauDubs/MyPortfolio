754171b6b6f5b436517d484aebb7a6fe0fb5a973
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-06 03:52:26 -0400

    refactored for links/routes, wired up basic home comp and css

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import Navigation from './components/Navigation';
+import Home from './components/Home';
 import Introduction from './components/Introduction';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="pBody">
-        <Navigation />
-        <Introduction />
-        <Projects />
-        <Contact />
+      <Navigation />
+      <Route 
+        path="/"
+        component={Home}
+        exact
+      />
+      <Route 
+        path="/intro"
+        component={Introduction}
+      />
+      <Route 
+        path="/projects"
+        component={Projects}
+      />
+      <Route 
+        path="/contact"
+        component={Contact}
+      />
       </div>
     </div>
   );
---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './componentCSS/home.css';
+
+const Home = () => {
+  return (
+    <div className="homeCont">
+
+    </div>
+  )
+}
+
+export default Home;
