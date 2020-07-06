d0fc0ea65fa93d1676820deecfde053a5869434b
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-06 04:15:08 -0400

    styled home, added animation

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -4,7 +4,8 @@ import './componentCSS/home.css';
 const Home = () => {
   return (
     <div className="homeCont">
-
+      <h1 className="homeTitle">Raine Wallace</h1>
+      <h2 className="homeSubTitle">Full-Stack Web Developer</h2>
     </div>
   )
 }
