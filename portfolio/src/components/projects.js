22b4f7883c2d88ea6a4dd222e2007ec200df4938
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-06 01:02:33 -0400

    wired up basic projects comp, added projects css file

---
+++ portfolio/src/components/projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './componentCSS/projects.css';
+
+
+const Projects = () => {
+  return (
+    <div>
+      <p>Projects here!</p>
+    </div>
+  )
+}
+
+export default Projects;
