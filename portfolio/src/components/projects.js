7e172561b8857b092c0b9abdbbd282db57c0c78e
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-06 01:32:25 -0400

    added project images to app and page

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -4,8 +4,28 @@ import './componentCSS/projects.css';
 
 const Projects = () => {
   return (
-    <div>
-      <p>Projects here!</p>
+    <div className="proCont">
+      <div className="proBox">
+        <img
+          className="proImg"
+          src="/images/RDRSiteImg.png"
+          alt="Rainy Day Resale's official site, Consignor Info"
+        />
+      </div>
+      <div className="proBox">
+        <img
+          className="proImg"
+          src="/images/LGTSiteImg.png"
+          alt="The 'Let's Get Tacos!' sun"
+        />
+      </div>
+      <div className="proBox">
+        <img
+          className="proImg"
+          src="/images/MTCSiteImg.png"
+          alt="TacoCat Escape! log-in page"
+        />
+      </div>
     </div>
   )
 }
